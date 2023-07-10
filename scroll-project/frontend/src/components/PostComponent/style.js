d00bbf5b2ad9b0704373a90cdbf663a9d2f70f5f
Ali <65356558+aliUbaydullayev5@users.noreply.github.com>
2023-07-10 20:09:10 +0500

    Add files via upload

---
+++ scroll-project/frontend/src/components/PostComponent/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+const PostBlock = styled.div`
+  min-height: 130px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  margin-bottom: 100px;
+  border-bottom: 2px solid black;
+  padding: 20px 10px;
+  
+  .title{
+    font-size: 26px;
+  }
+  .body{
+    
+  }
+`
+
+export default PostBlock
